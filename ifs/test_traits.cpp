109d7d236cdba941d826ae30f5e9b6719ab62b92
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-01-15 12:53:59 +0000

    Type traits

---
+++ ifs/test_traits.cpp
@@ -0,0 +1,117 @@
+#include <catch2/catch_test_macros.hpp>
+#include <iostream>
+
+int foo(int x)
+{
+    return x * 2;
+}
+
+template <size_t N>
+struct Foo
+{
+    static constexpr size_t value = N * 2;
+};
+
+template <typename T>
+struct Identity
+{
+    using type = T;
+};
+
+TEST_CASE("type traits")
+{
+    REQUIRE(foo(8) == 16);
+    static_assert(Foo<8>::value == 16);
+
+    Identity<int>::type x = 42;
+}
+
+////////////////////////////////////////
+// IsVoid
+
+template <typename T, T v>
+struct IntegralConstant
+{
+    static constexpr T value = v;
+};
+
+static_assert(IntegralConstant<int, 5>::value == 5);
+
+template <bool Value>
+using BoolConstant = IntegralConstant<bool, Value>;
+
+using TrueType = BoolConstant<true>;
+using FalseType = BoolConstant<false>;
+static_assert(TrueType::value == true);
+static_assert(FalseType::value == false);
+
+template <typename T>
+struct IsVoid : FalseType
+{
+    //static constexpr bool value = false;
+};
+
+template <>
+struct IsVoid<void> : TrueType
+{
+    //static constexpr bool value = true;
+};
+
+// template  variable
+template <typename T>
+constexpr bool IsVoid_v = IsVoid<T>::value;
+
+TEST_CASE("is_void")
+{
+    using TypeA = int;
+    using TypeB = void;
+
+    static_assert(IsVoid<TypeA>::value == false);
+    static_assert(IsVoid<TypeB>::value == true);
+    static_assert(std::is_void_v<TypeB> == true);
+}
+
+/////////////////////////////////////////////////////
+
+template <typename T>
+struct IsPointer : FalseType
+{};
+
+template <typename T>
+struct IsPointer<T*> : TrueType
+{};
+
+// template  variable
+template <typename T>
+constexpr bool IsPointer_v = IsPointer<T>::value;
+
+template <typename T>
+void print_value(T v)
+{
+    if constexpr(IsPointer_v<T>)
+    {
+        assert(v != nullptr);
+        std::cout << "value: " << *v << "\n";
+    }
+    else
+    {
+        std::cout << "value: " << v << "\n";
+    }
+}
+
+TEST_CASE("is_pointer")
+{
+    using TypeA = int;
+    using TypeB = int*;
+    using TypeC = std::string*;
+
+    static_assert(IsPointer_v<TypeA> == false);
+    static_assert(IsPointer_v<TypeB> == true);
+    static_assert(IsPointer_v<TypeC> == true);
+
+    int x = 10;
+    int* ptr_x = &x;
+
+    print_value(x);
+    print_value(ptr_x);
+}
--- ifs/tests_ifs.cpp
+++ ifs/tests_ifs.cpp
@@ -8,6 +8,7 @@
 #include <queue>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -15,9 +16,7 @@ TEST_CASE("if with initializer")
 {
     std::vector vec = {1, 2, 543, 235, 534, 665, 654, 3456};
 
-    auto pos = std::find(begin(vec), end(vec), 665);
-
-    if (pos != end(vec))
+    if (auto pos = std::find(begin(vec), end(vec), 665); pos != end(vec))
     {
         std::cout << "Found: " << *pos << "\n";
     }
@@ -26,4 +25,78 @@ TEST_CASE("if with initializer")
         std::cout << "Item not found!\n";
         assert(pos == end(vec));
     }
+}
+
+TEST_CASE("if with initializers & structured bindings")
+{
+    std::map<int, std::string> dict = {{1, "one"}};
+
+    if (auto [pos, was_inserted] = dict.emplace(2, "two"); was_inserted)
+    {
+        const auto& [key, value] = *pos;
+        std::cout << "Item: " << key << " - " << value << " was inserted\n";
+    }
+    else
+    {
+        std::cout << "Insert was not executed...\n";
+    }
+}
+
+TEST_CASE("ifs with mutex")
+{
+    std::queue<std::string> qmsg;
+    std::mutex mtx_qmsg;
+
+    SECTION("thread#1")
+    {
+        std::lock_guard lk{mtx_qmsg};
+        qmsg.push("START");
+    }
+
+    SECTION("thread#2")
+    {
+        std::string msg;
+
+        if (std::lock_guard lk{mtx_qmsg}; !qmsg.empty())
+        {
+            msg = qmsg.front();
+            qmsg.pop();
+        }
+
+        std::cout << msg << "\n";
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+// constexpr if
+
+template <typename T>
+bool is_power_of_2(T value)
+{
+    return value > 0 && (value & (value - 1)) == 0;
+}
+
+bool is_power_of_2(double value)
+{
+    int exponent;
+    const double mantissa = std::frexp(value, &exponent);
+    return mantissa == double(0.5);
+}
+
+bool is_power_of_2(float value)
+{
+    int exponent;
+    const float mantissa = std::frexp(value, &exponent);
+    return mantissa == float(0.5);
+}
+
+TEST_CASE("constexpr if")
+{
+    REQUIRE(is_power_of_2(8));
+    REQUIRE(is_power_of_2(16));
+    REQUIRE(is_power_of_2(128));
+    REQUIRE(is_power_of_2(129) == false);
+
+    REQUIRE(is_power_of_2(8.0));
+    REQUIRE(is_power_of_2(8.0f));
 }
