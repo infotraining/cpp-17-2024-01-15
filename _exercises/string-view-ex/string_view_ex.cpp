037d8babc5d10333b5773077747b3487a2f4f9a7
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-01-17 12:25:35 +0000

    string_view

--- _exercises/string-view-ex/string_view_ex.cpp
+++ _exercises/string-view-ex/string_view_ex.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
+#include <ranges>
 #include <set>
 #include <string>
 #include <string_view>
@@ -8,26 +9,73 @@
 
 using namespace std;
 
-// TODO
+template <typename Container>
+void print_all(const Container& container, std::string_view prefix)
+{
+    cout << prefix << ": [ ";
+    for (const auto& item : container)
+        cout << item << " ";
+    cout << "]\n";
+}
 
-// TEST_CASE("split with spaces")
-// {
-//     const char* text = "one two three four";
+std::vector<std::string_view> split_text(std::string_view text, std::string_view pattern = ", ")
+{
+    std::vector<std::string_view> result{};
 
-//     std::vector<std::string_view> words = split_text(text);
+    size_t start{};
+    size_t end{};
 
-//     auto expected = {"one", "two", "three", "four"};
+    while (end != std::string_view::npos)
+    {
+        end = text.find_first_of(pattern, start);
+        std::string_view token = text.substr(start, end - start);
+        result.push_back(token);
+        start = text.find_first_not_of(pattern, end);
+    }
 
-//     REQUIRE(equal(begin(expected), end(expected), begin(words)));
-// }
+    return result;
+}
 
-// TEST_CASE("split with commas")
-// {
-//     string text = "one,two,three,four";
+namespace Cpp20
+{
+    std::vector<std::string_view> split_text(std::string_view text, std::string_view pattern = " ")
+    {
+        std::vector<std::string_view> result{};
 
-//     auto words = split_text(text);
+        auto tokens = std::views::split(text, pattern);
 
-//     auto expected = {"one", "two", "three", "four"};
+        for (auto&& token : tokens)
+        {
+            std::string_view token_sv{token.begin(), token.size()};
+            result.push_back(token_sv);
+        }
 
-//     REQUIRE(equal(begin(expected), end(expected), begin(words)));
-// }
+        return result;
+    }
+}
+
+TEST_CASE("split with spaces")
+{
+    const char* text = "one two three four";
+
+    std::vector<std::string_view> words = Cpp20::split_text(text);
+
+    print_all(words, "words");
+
+    auto expected = {"one", "two", "three", "four"};
+
+    REQUIRE(equal(begin(expected), end(expected), begin(words)));
+}
+
+TEST_CASE("split with commas")
+{
+    string text = "one,two,three,four";
+
+    auto words = Cpp20::split_text(text, ",");
+
+    print_all(words, "words");
+
+    auto expected = {"one", "two", "three", "four"};
+
+    REQUIRE(equal(begin(expected), end(expected), begin(words)));
+}
